0dc9342f723968160c2bfdde6347b7ca3a508d48
anna011001 <anna.bissinger8779@gmail.com>
2025-12-22 22:16:55 +0100

    nastavak 5 zadatka

--- WA3 - Razmjena podataka između klijenta i poslužitelja/app/pizza-express/routes/narudzbe.js
+++ WA3 - Razmjena podataka između klijenta i poslužitelja/app/pizza-express/routes/narudzbe.js
@@ -2,7 +2,30 @@
 
 import express from 'express';
 import { db } from '../index.js';
-const narudzbeRouter = express.Router();
+import { ObjectId } from 'mongodb';
+export const narudzbeRouter = express.Router();
+
+narudzbeRouter.get('/', async (req, res) => {
+    let narudzbe_collection = db.collection('narudzbe');
+    let narudzbe = await narudzbe_collection.find().toArray();
+
+    if (narudzbe.length === 0) {
+        return res.status(404).json({ error: 'Nema narudzbi' });
+    }
+    res.status(200).json(narudzbe);
+});
+
+narudzbeRouter.get('/:id', async (req, res) => {
+    let narudzbe_collection = db.collection('narudzbe');
+    let id_param = req.params.id;
+    let narudzba = await narudzbe_collection.findOne({ _id: new ObjectId(id_param) });
+
+    if (!narudzba) {
+        return res.status(404).json({ error: 'Narudzba nije pronadena' });
+    }
+
+    res.status(200).json(narudzba);
+});
 
 // POST /narudzbe - Izrada nove narudžbe pizza
 narudzbeRouter.post('/', async (req, res) => {
@@ -22,10 +45,15 @@ narudzbeRouter.post('/', async (req, res) => {
 
     if (
         !novaNarudzba.narucene_pizze.every(stavka => {
-            return Number.isInteger(stavka.količina) && stavka.količina > 0 && ['mala', 'srednja', 'velika'].includes(stavka.veličina);
+            return Number.isInteger(stavka.količina) && stavka.količina > 0 && ['mala', 'srednja', 'velika'].includes(stavka.veličina)
+            && typeof novaNarudzba.naziv !== "string";
         })
     ) {
         return res.status(400).json({ error: 'Neispravni podaci u stavci narudzbe' });
+    }
+
+    if (isNaN(novaNarudzba.broj_telefona)) {
+        return res.status(400).send("Telefon mora biti broj.");
     }
 
     let pizze_collection = db.collection('pizze');
@@ -35,12 +63,41 @@ narudzbeRouter.post('/', async (req, res) => {
         return res.status(400).json({ error: 'Odabrali ste pizzu koju nemamo u ponudi' });
     }
 
+    let ukupna_cijena = 0;
+
+    novaNarudzba.narucene_pizze.forEach(stavka => {
+        const pizza = dostupne_pizze.find(druga_pizza => druga_pizza.naziv == stavka.naziv);
+
+        ukupna_cijena += pizza.cijene[stavka.veličina] * stavka.količina;
+    });
+
+    novaNarudzba.ukupna_cijena = ukupna_cijena;
+    
     try {
         let result = await narudzbe_collection.insertOne(novaNarudzba);
         res.status(201).json({ insertedId: result.insertedId });
     } catch (error) {
         console.log(error.errorResponse);
         res.status(400).json({ error: error.errorResponse });
+    }
+});
+
+narudzbeRouter.patch('/:id', async (req, res) => {
+    let narudzbe_collection = db.collection('narudzbe');
+    let id_param = req.params.id;
+    let noviStatus = req.body.status;
+
+    try {
+        let result = await narudzbe_collection.updateOne({ _id: new ObjectId(id_param) }, { $set: { status: noviStatus } });
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ error: 'Narudzba nije pronadena' });
+        }
+
+        res.status(200).json({ modifiedCount: result.modifiedCount });
+    } catch (error) {
+        console.log(error.errorResponse);
+        res.status(400).json({ error: error.errorResponse});
     }
 });
 
--- WA3 - Razmjena podataka između klijenta i poslužitelja/app/pizza-express/routes/pizze.js
+++ WA3 - Razmjena podataka između klijenta i poslužitelja/app/pizza-express/routes/pizze.js
@@ -6,8 +6,24 @@ const pizzeRouter = express.Router();
 // GET /pizze - Dohvaćanje svih pizza (npr. GET /pizze)
 pizzeRouter.get('/', async (req, res) => {
     let pizze_collection = db.collection('pizze'); // referenca na kolekciju 'pizze'
-    let allPizze = await pizze_collection.find().toArray(); // pretvorba u Array
-    res.status(200).json(allPizze);
+    let cijena_query = req.query.cijena;
+    let naziv_query = req.query.naziv;
+
+    if(!cijena_query) {
+        let pizze = await pizze_collection.find().toArray();
+        return res.status(200).json(pizze);
+    }
+
+    try {
+        let pizze = await pizze_collection
+            .find()
+            .sort({ cijena: Number(cijena_query), naziv: Number(naziv_query) })
+            .toArray(); // pretvorba u Array
+        res.status(200).json(pizze);
+    } catch (error) {
+        console.log(error.errorResponse);
+        res.status(400).json({ error: error.errorResponse });
+    }
 });
 
 // GET /pizze/:naziv - Dohvaćanje pizze prema nazivu (npr. GET /pizze/Margherita)
@@ -22,6 +38,33 @@ pizzeRouter.get('/:naziv', async (req, res) => {
 pizzeRouter.post('/', async (req, res) => {
     let pizze_collection = db.collection('pizze');
     let novaPizza = req.body;
+    let obavezniKljucevi = ["naziv", "sastojci", "cijene", "slika_url"];
+    let obavezniKljuceviCijene = ["mala", "srednja", "jumbo"];
+
+    if (!obavezniKljucevi.every(kljuc => kljuc in novaPizza)) {
+        return res.status(400).send("Niste poslali sve parametre!");
+    }
+
+    if (!obavezniKljuceviCijene.every(kljuc => kljuc in novaPizza.cijene)) {
+        return res.status(400).send("Niste poslali sve parametre!");
+    }
+
+    if (isNaN(novaPizza.cijene.mala) || isNaN(novaPizza.cijene.srednja || isNaN(novaPizza.cijene.jumbo))) {
+        return res.status(400).send("Cijena pizze mora biti broj!");
+    }
+
+    if (novaPizza.sastojci.some(sastojak => typeof sastojak !== "string")) {
+        return res.status(400).send("Sastojci pizze moraju biti rijeci!");
+    }
+
+    if (typeof novaPizza.slika_url !== "string" || novaPizza.slika_url.trim() == "") {
+        return res.status(400).send("Url pizze mora biti string i ne smije biti prazan.");
+    }
+
+    if (typeof novaPizza.naziv !== "string") {
+        return res.status(400).send("Naziv pizze mora biti rijec!");
+    }
+
     try {
         let result = await pizze_collection.insertOne(novaPizza);
         res.status(201).json({ InsertedId: result.insertedId });
@@ -32,6 +75,68 @@ pizzeRouter.post('/', async (req, res) => {
     }
 });
 
+pizzeRouter.patch('/', async (req, res) => {
+    let pizze_collection = db.collection('pizze');
 
+    try {
+        let result = await pizze_collection.updateMany({ cijena: { $lt: 15 } }, { $inc: { cijena: 2 } }); // povećaj cijenu svih pizza čija je cijena manja od 15 za 2 eura
+        res.status(200).json({ modifiedCount: result.modifiedCount });
+    } catch (error) {
+        console.log(error.errorResponse);
+        res.status(400).json({ error: error.errorResponse });
+    }
+});
+
+pizzeRouter.patch('/:naziv', async (req, res) => {
+    let pizze_collection = db.collection('pizze');
+    let naziv_param = req.params.naziv;
+    let novaCijena = req.body.cijena;
+
+    try {
+        let result = await pizze_collection.updateOne({ naziv: naziv_param }, { $set: { cijena: novaCijena } });
+        res.status(200).json({ modifiedCount: result.modifiedCount });
+    } catch (error) {
+        console.log(error.errorResponse);
+        res.status(400).json({ error: error.errorResponse });
+    }
+});
+
+pizzeRouter.put('/', async (req, res) => {
+    let pizze_collection = db.collection('pizze');
+    let noviMeni = req.body;
+
+    try{
+        await pizze_collection.deleteMany({});
+        let result = await pizze_collection.insertMany(noviMeni);
+        res.status(200).json({ insertedCount: result.insertedCount });
+    } catch (error) {
+        console.log(error.errorResponse);res.status(400).json({ error: error.errorResponse });
+    }
+});
+
+pizzeRouter.delete('/:naziv', async (req, res) => {
+    let pizze_collection = db.collection('pizze');
+    let naziv_param = req.params.naziv;
+
+    try {
+        let result = await pizze_collection.deleteOne({ naziv: naziv_param }); // brišemo pizzu prema nazivu
+        res.status(200).json({ deletedCount: result.deletedCount });
+    } catch (error) {
+        console.log(error.errorResponse);
+        res.status(400).json({ error: error.errorResponse });
+    }
+});
+
+pizzeRouter.delete('/', async (req, res) => {
+    let pizze_collection = db.collection('pizze');
+
+    try {
+        let result = await pizze_collection.deleteMany({});
+        res.status(200).json({ deletedCount: result.deletedCount });
+    } catch (error) {
+        console.log(error.errorResponse);
+        res.status(400).json({ error: error.errorResponse });
+    }
+});
 
 export default pizzeRouter;
